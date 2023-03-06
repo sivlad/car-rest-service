b111199a662e1b9e53f2af3d487fe3fdef822e0a
sivlad <66171079+sivlad@users.noreply.github.com>
2023-03-06 21:28:47 +0200

    add starter

--- src/main/java/ua/com/foxmined/carrestservice/AppStarter.java
+++ src/main/java/ua/com/foxmined/carrestservice/AppStarter.java
@@ -2,24 +2,21 @@ package ua.com.foxmined.carrestservice;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 import ua.com.foxmined.carrestservice.utils.CarDBInitializer;
 
 @Log4j2
-@Configuration
+@Component
 public class AppStarter {
 
     @Autowired
     private CarDBInitializer carDBInitializer;
 
-    @Bean
-    public ApplicationRunner init() {
+    @EventListener(ApplicationReadyEvent.class)
+    public void AppStarter() {
         log.info("ApplicationRunner has started");
-        return args -> {
-            carDBInitializer.createRowsInDb();
-        };
+        carDBInitializer.createRowsInDb();
     }
-
 }
